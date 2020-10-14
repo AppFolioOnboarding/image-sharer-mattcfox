f8bf6ee8cd6402494e37e2ee5388bd0effe63c9c
Matt Fox <matthew.fox@appfolio.com>
2020-10-14 16:22:21 -0700

    feat: Add mechanism to upload image urls
    This includes basic url validation

---
+++ app/models/image.rb
@@ -0,0 +1,13 @@
+class Image < ApplicationRecord
+  validates(
+    :url,
+    format: {
+      with: Regexp.new(
+        '\Ahttps?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b'\
+        '([-a-zA-Z0-9()@:%_\+.~#?&//=]*)',
+        Regexp::IGNORECASE
+      )
+    },
+    presence: true
+  )
+end
